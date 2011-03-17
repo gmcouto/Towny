eb31534c65bcd46ed95bf833151a0cae4f433c1a
Gabriel Couto <gmcouto@gmail.com>
2011-03-17 00:58:08 -0300

    Fix for right Coordinates

--- src/ca/xshade/bukkit/towny/object/Coord.java
+++ src/ca/xshade/bukkit/towny/object/Coord.java
@@ -1,5 +1,6 @@
 package ca.xshade.bukkit.towny.object;
 
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
@@ -11,81 +12,94 @@ import org.bukkit.entity.Entity;
  * @author Shade
  */
 public class Coord {
-	protected static int cellSize = 16;
-	protected int x, z;
-	
-	public Coord(int x, int z) {
-		this.x = x;
-		this.z = z;
-	}
-	
-	public Coord(Coord coord) {
-		this.x = coord.getX();
-		this.z = coord.getZ();
-	}
 
-	public int getX() {
-		return x;
-	}
+    protected static int cellSize = 16;
+    protected int x, z;
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    public Coord(int x, int z) {
+        this.x = x;
+        this.z = z;
+        System.out.println("NEW COORD X: " + x + "    Z: " + z);
+    }
 
-	public int getZ() {
-		return z;
-	}
+    public Coord(Coord coord) {
+        this.x = coord.getX();
+        this.z = coord.getZ();
+    }
 
-	public void setZ(int z) {
-		this.z = z;
-	}
+    public int getX() {
+        return x;
+    }
 
-	@Override
-	public int hashCode() {
-		int result = 17;
-		result = 31 * result + x;
-		result = 31 * result + z;
-		return result;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == this)
-			return true;
-		if (!(obj instanceof Coord))
-			return false;
+    public int getZ() {
+        return z;
+    }
 
-		Coord o = (Coord) obj;
-		return this.x == o.x && this.z == o.z;
-	}
+    public void setZ(int z) {
+        this.z = z;
+    }
 
-	public static Coord parseCoord(int x, int z) {
-		return new Coord(x / getCellSize() - (x < 0 ? 1 : 0),
-				z / getCellSize() - (z < 0 ? 1 : 0));
-	}
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + x;
+        result = 31 * result + z;
+        return result;
+    }
 
-	public static Coord parseCoord(Entity entity) {
-		return parseCoord(entity.getLocation());
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Coord)) {
+            return false;
+        }
 
-	public static Coord parseCoord(Location loc) {
-		return parseCoord(loc.getBlockX(), loc.getBlockZ());
-	}
+        Coord o = (Coord) obj;
+        return this.x == o.x && this.z == o.z;
+    }
 
-	public static Coord parseCoord(Block block) {
-		return parseCoord(block.getX(), block.getZ());
-	}
+    public static Coord parseCoord(int x, int z) {
+        int xresult = x / getCellSize();
+        double xdresult = (x * 1.0) / getCellSize();
+        int zresult = z / getCellSize();
+        double zdresult = (z * 1.0) / getCellSize();
+        boolean xneedfix = (((double) xresult) != xdresult);
+        boolean zneedfix = (((double) zresult) != zdresult);
+        return new Coord(xresult - ((x < 0 && xneedfix) ? 1 : 0),
+                zresult - ((z < 0 && zneedfix) ? 1 : 0));
+    }
 
-	@Override
-	public String toString() {
-		return getX() + "," + getZ();
-	}
+    public static Coord parseCoord(Entity entity) {
+        Chunk c = entity.getWorld().getChunkAt(entity.getLocation());
+        return new Coord(c.getX(), c.getZ());
+    }
 
-	public static void setCellSize(int cellSize) {
-		Coord.cellSize = cellSize;
-	}
+    public static Coord parseCoord(Location loc) {
+        Chunk c = loc.getWorld().getChunkAt(loc);
+        return new Coord(c.getX(), c.getZ());
+    }
 
-	public static int getCellSize() {
-		return cellSize;
-	}
+    public static Coord parseCoord(Block block) {
+        Chunk c = block.getWorld().getChunkAt(block.getLocation());
+        return new Coord(c.getX(), c.getZ());
+    }
+
+    @Override
+    public String toString() {
+        return getX() + "," + getZ();
+    }
+
+    public static void setCellSize(int cellSize) {
+        Coord.cellSize = cellSize;
+    }
+
+    public static int getCellSize() {
+        return cellSize;
+    }
 }
